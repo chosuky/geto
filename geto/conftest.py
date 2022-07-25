2295d76ad5b75287e3755338f56ddbb747cb9abe
sechcho <sechcho@SECHCHO-7320.kr.oracle.com>
2022-07-25 19:25:14 +0900

    first commit

---
+++ geto/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from geto.users.models import User
+from geto.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
